82b75713d29528084a992c033adb48ba4af6c9ea
suhongkim98 <kshz9815@naver.com>
2023-05-01 18:03:15 +0900

    feature(PET-35) : NotFoundBoard 런타임 익셉션 핸들러 구현

--- core/core-api/src/main/kotlin/com/thepetto/core/api/global/exception/CustomExceptionCode.kt
+++ core/core-api/src/main/kotlin/com/thepetto/core/api/global/exception/CustomExceptionCode.kt
@@ -10,5 +10,5 @@ enum class CustomExceptionCode(
     REQUEST_PARAMETER_BIND_FAILED(HttpStatus.BAD_REQUEST, "REQ_001", "PARAMETER_BIND_FAILED"),
     DUPLICATE_MEMBER_EXCEPTION(HttpStatus.CONFLICT, "AUTH_001", "회원 중복"),
     INVALID_REFRESH_TOKEN(HttpStatus.FORBIDDEN, "AUTH_002", "리프레시 토큰이 유효하지 않습니다"),
-    NOT_FOUND_ACCOUNT(HttpStatus.NOT_FOUND, "AUTH_003", "회원을 찾을 수 없음"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "STATE_001", "찾을 수 없음"),
 }
