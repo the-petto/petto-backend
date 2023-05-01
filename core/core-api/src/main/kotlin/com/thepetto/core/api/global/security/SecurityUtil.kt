d4f833fa3d4f990bb44a9bfe82223512026fb6de
suhongkim98 <kshz9815@naver.com>
2023-05-01 18:03:50 +0900

    feature(PET-35) : 시큐리티 유틸 로직 수정

--- core/core-api/src/main/kotlin/com/thepetto/core/api/global/security/SecurityUtil.kt
+++ core/core-api/src/main/kotlin/com/thepetto/core/api/global/security/SecurityUtil.kt
@@ -1,22 +1,13 @@
 package com.thepetto.core.api.global.security
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 
 
 object SecurityUtil {
-    private val logger: Logger = LoggerFactory.getLogger(SecurityUtil::class.java)
-
-    fun currentUsername(): String? {
-        val authentication: Authentication? = SecurityContextHolder.getContext().authentication
-            ?: null
-        if (authentication == null) {
-            logger.debug("Security Context에 인증 정보가 없습니다.")
-            return null
-        }
+    fun currentUsername(): String {
+        val authentication: Authentication = SecurityContextHolder.getContext().authentication
 
         var username: String? = null
         if (authentication.principal is UserDetails) {
@@ -25,6 +16,6 @@ object SecurityUtil {
         } else if (authentication.principal is String) {
             username = authentication.principal.toString()
         }
-        return username
+        return username ?: throw IllegalStateException()
     }
 }
