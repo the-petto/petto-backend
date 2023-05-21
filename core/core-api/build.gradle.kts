bf1fe8f9f97735a91b35b76ba757a84ed1dda81f
suhongkim98 <kshz9815@naver.com>
2023-05-21 19:27:55 +0900

    feature(PET-48) : 산책해주세요 API controller 단위테스트 작성

--- core/core-api/build.gradle.kts
+++ core/core-api/build.gradle.kts
@@ -1,16 +1,15 @@
 dependencies {
     implementation(project(":core:core-enum"))
     implementation(project(":support:swagger"))
+    implementation(project(":support:kotest"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
 
-
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
